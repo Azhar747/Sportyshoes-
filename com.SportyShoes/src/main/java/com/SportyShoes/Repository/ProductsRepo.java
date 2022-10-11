4a1273b5b0d49081b2fbd6a2c2a27de509da0a92
Azhar747 <92854137+Azhar747@users.noreply.github.com>
2022-10-11 17:20:32 +0530

    Delete com.SportyShoes directory

--- com.SportyShoes/src/main/java/com/SportyShoes/Repository/ProductsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.SportyShoes.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.SportyShoes.Entity.Product;
-
-public interface ProductsRepo extends CrudRepository<Product, Integer>{
-	
-}
