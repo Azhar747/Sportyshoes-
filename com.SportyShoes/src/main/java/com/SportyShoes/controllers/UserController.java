df5ccccb4311ba129b283f521499516bb69bd38b
Azhar <mirazhar747@gmail.com>
2022-10-12 17:43:19 +0530

    Oct12,1723 Commit

--- com.SportyShoes/src/main/java/com/SportyShoes/controllers/UserController.java
+++ com.SportyShoes/src/main/java/com/SportyShoes/controllers/UserController.java
@@ -1,11 +1,22 @@
 package com.SportyShoes.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.SportyShoes.Entity.Users;
+import com.SportyShoes.service.Userserivce;
+
 @Controller
 public class UserController {
+	
+	@Autowired
+	Userserivce userser;
 	
 	@PostMapping("/Adduser")
 	public String adduser() {
@@ -13,12 +24,20 @@ public class UserController {
 		
 	}
 	@GetMapping("/User")
-	public String user() {
+	public String user(Model m) {
+		
+		List<Users>listuser= userser.getallusers();
+		m.addAttribute("listuser", listuser);
+		
 		return "User.html";
 		}
 	
 	@PostMapping("/SaveUser")
-	public void saveuser() {
+	public String saveuser(Users user) {
+		
+		userser.Usersave(user);
+		
+		return "Saveuser.html";
 		
 	}
 	
--- com.SportyShoes/src/main/java/com/SportyShoes/service/Userserivce.java
+++ com.SportyShoes/src/main/java/com/SportyShoes/service/Userserivce.java
@@ -1,10 +1,15 @@
 package com.SportyShoes.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 
 import com.SportyShoes.Entity.Users;
 import com.SportyShoes.Repository.UserRepo;
-
+@Service
 public class Userserivce {
    
 	@Autowired
@@ -13,6 +18,13 @@ public class Userserivce {
 	public void Usersave(Users user) {
 		
 		userrepo.save(user);
+		
+	}
+	public List<Users> getallusers() {
+		List<Users> listuser = new ArrayList<>();
+		userrepo.findAll().forEach(m -> listuser.add(m));
+		return listuser;
+	
 		
 	}
 }
